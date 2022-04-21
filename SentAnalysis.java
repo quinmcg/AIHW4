2068d191526a7af227d81db2a0a8ad6d280452b8
Quinlan McGaugh <jmcgaugh@middlebury.edu>
2022-04-21 19:25:14 -0400

    File Upload

---
+++ SentAnalysis.java
@@ -0,0 +1,117 @@
+/*
+ * Please see submission instructions for what to write here. 
+ */
+
+import java.io.*;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class SentAnalysis {
+
+	final static File TRAINFOLDER = new File("train-s21");
+	
+		
+	public static void main(String[] args) throws IOException
+	{	
+		ArrayList<String> files = readFiles(TRAINFOLDER);		
+		
+		train(files);
+		//if command line argument is "evaluate", runs evaluation mode
+		if (args.length==1 && args[0].equals("evaluate")){
+			evaluate();
+		}
+		else{//otherwise, runs interactive mode
+			@SuppressWarnings("resource")
+			Scanner scan = new Scanner(System.in);
+			System.out.print("Text to classify>> ");
+			String textToClassify = scan.nextLine();
+			System.out.println("Result: "+classify(textToClassify));
+		}
+		
+	}
+	
+
+	
+	/*
+	 * Takes as parameter the name of a folder and returns a list of filenames (Strings) 
+	 * in the folder.
+	 */
+	public static ArrayList<String> readFiles(File folder){
+		
+		System.out.println("Populating list of files");
+		
+		//List to store filenames in folder
+		ArrayList<String> filelist = new ArrayList<String>();
+		
+	
+		for (File fileEntry : folder.listFiles()) {
+	        String filename = fileEntry.getName();
+	        filelist.add(filename);
+		}
+	    
+		/*
+		for (String fileEntry : filelist) {
+	        System.out.println(fileEntry);
+		}
+		
+		System.out.println(filelist.size());
+		*/
+		
+		
+		return filelist;
+	}
+	
+	
+
+	
+	/*
+	 * TO DO
+	 * Trainer: Reads text from data files in folder datafolder and stores counts 
+	 * to be used to compute probabilities for the Bayesian formula.
+	 * You may modify the method header (return type, parameters) as you see fit.
+	 */
+	public static void train(ArrayList<String> files) throws FileNotFoundException
+	{
+		
+	}
+
+
+	/*
+	 * Classifier: Classifies the input text (type: String) as positive or negative
+	 */
+	public static String classify(String text)
+	{
+		String result="";
+		
+		return result;
+		
+	}
+	
+	
+	
+
+	/*
+	 * TO DO
+	 * Classifier: Classifies all of the files in the input folder (type: File) as positive or negative
+	 * You may modify the method header (return type, parameters) as you like.
+	 */
+	public static void evaluate() throws FileNotFoundException 
+	{
+		@SuppressWarnings("resource")
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("Enter folder name of files to classify: ");
+		String foldername = scan.nextLine();
+		File folder = new File(foldername);
+		
+		ArrayList<String> filesToClassify = readFiles(folder);
+		
+		
+		
+	}
+	
+	
+	
+}
