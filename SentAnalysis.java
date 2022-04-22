2a56fdac1eb96d9f484fd80f473d8f57753363b7
Hokalaka2 <otis.true@gmail.com>
2022-04-21 21:10:41 -0400

    Update SentAnalysis.java

--- SentAnalysis.java
+++ SentAnalysis.java
@@ -1,5 +1,5 @@
 /*
- * Please see submission instructions for what to write here. 
+ * Please see submission instructions for what to write here.
  */
 
 import java.io.*;
@@ -12,11 +12,10 @@ public class SentAnalysis {
 
 	final static File TRAINFOLDER = new File("testingtrain");
 	
-		
 	public static void main(String[] args) throws IOException
-	{	
-		ArrayList<String> files = readFiles(TRAINFOLDER);		
-		
+	{
+		ArrayList<String> files = readFiles(TRAINFOLDER);
+
 		train(files);
 		//if command line argument is "evaluate", runs evaluation mode
 		// if (args.length==1 && args[0].equals("evaluate")){
@@ -29,46 +28,46 @@ public class SentAnalysis {
 		// 	String textToClassify = scan.nextLine();
 		// 	System.out.println("Result: "+classify(textToClassify));
 		// }
-		
-	}
-	
 
-	
+	}
+
+
+
 	/*
-	 * Takes as parameter the name of a folder and returns a list of filenames (Strings) 
+	 * Takes as parameter the name of a folder and returns a list of filenames (Strings)
 	 * in the folder.
 	 */
 	public static ArrayList<String> readFiles(File folder){
-		
+
 		System.out.println("Populating list of files");
-		
+
 		//List to store filenames in folder
 		ArrayList<String> filelist = new ArrayList<String>();
-		
-	
+
+
 		for (File fileEntry : folder.listFiles()) {
 	        String filename = fileEntry.getName();
 	        filelist.add(filename);
 		}
-	    
+
 		/*
 		for (String fileEntry : filelist) {
 	        System.out.println(fileEntry);
 		}
-		
+
 		System.out.println(filelist.size());
 		*/
-		
-		
+
+
 		return filelist;
 	}
-	
-	
 
-	
+
+
+
 	/*
 	 * TO DO
-	 * Trainer: Reads text from data files in folder datafolder and stores counts 
+	 * Trainer: Reads text from data files in folder datafolder and stores counts
 	 * to be used to compute probabilities for the Bayesian formula.
 	 * You may modify the method header (return type, parameters) as you see fit.
 	 */
@@ -124,36 +123,36 @@ public class SentAnalysis {
 	public static String classify(String text)
 	{
 		String result="";
-
+		
 		result = evaluate(text);
-		
+
 		return result;
-		
+
 	}
-	
-	
-	
+
+
+
 
 	/*
 	 * TO DO
 	 * Classifier: Classifies all of the files in the input folder (type: File) as positive or negative
 	 * You may modify the method header (return type, parameters) as you like.
 	 */
-	public static void evaluate() throws FileNotFoundException 
+	public static void evaluate() throws FileNotFoundException
 	{
 		@SuppressWarnings("resource")
 		Scanner scan = new Scanner(System.in);
-		
+
 		System.out.print("Enter folder name of files to classify: ");
 		String foldername = scan.nextLine();
 		File folder = new File(foldername);
-		
+
 		ArrayList<String> filesToClassify = readFiles(folder);
-		
-		
-		
+
+
+
 	}
-	
-	
-	
+
+
+
 }
