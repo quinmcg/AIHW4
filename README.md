e1411cafe0ff264b831faeb4a6ff460fbefca8bb
Quinlan McGaugh <jmcgaugh@middlebury.edu>
2022-04-24 23:41:16 -0400

    Classify Finished
    Part I

--- README.md
+++ README.md
@@ -3,3 +3,18 @@ Artificial Intelligence HW4 - Naive Bayes Classifier
 
 
 Authors: Quinlan McGaugh and Otis Milliken
+
+
+Reflection (Part I):
+
+Failures:
+
+1. "I hate my AI class!"
+This one fails because, somewhat surprisingly, "ai" is a very positive word according to our training data. We see the probability of "AI" given positive is nearly 10x the probability of seeing "AI" given negative:
+"AI" | Neg: 3.1044543985029585E-5
+"AI" | Pos: 1.7849849745647407E-4
+
+It was also difficult because the text was only five words, two of which were "filler" words (I and my), which were roughly even probabilities between positive and negative.
+
+
+2. 
--- SentAnalysis.java
+++ SentAnalysis.java
@@ -8,6 +8,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 import java.util.regex.Pattern;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class SentAnalysis {
 
@@ -17,7 +20,7 @@ public class SentAnalysis {
 	protected static int pos_count = 0;
 	protected static int neg_count = 0;
 
-	final static File TRAINFOLDER = new File("testingtrain");
+	final static File TRAINFOLDER = new File("train");
 
 	public static void main(String[] args) throws IOException
 	{
@@ -25,16 +28,16 @@ public class SentAnalysis {
 
 		train(files);
 		//if command line argument is "evaluate", runs evaluation mode
-		// if (args.length==1 && args[0].equals("evaluate")){
-		// 	evaluate();
-		// }
-		// else{//otherwise, runs interactive mode
-		// 	@SuppressWarnings("resource")
-		// 	Scanner scan = new Scanner(System.in);
-		// 	System.out.print("Text to classify>> ");
-		// 	String textToClassify = scan.nextLine();
-		// 	System.out.println("Result: "+classify(textToClassify));
-		// }
+		if (args.length==1 && args[0].equals("evaluate")){
+			evaluate();
+		}
+		else{//otherwise, runs interactive mode
+			@SuppressWarnings("resource")
+			Scanner scan = new Scanner(System.in);
+			System.out.print("Text to classify>> ");
+			String textToClassify = scan.nextLine();
+			System.out.println("Result: "+classify(textToClassify));
+		}
 
 	}
 
@@ -93,8 +96,8 @@ public class SentAnalysis {
 			}
 		}
 
-		System.out.println("Positive Hashmap: " + word_count_pos);
-		System.out.println("Negative Hashmap: " + word_count_neg);
+		//System.out.println("Positive Hashmap: " + word_count_pos);
+		//System.out.println("Negative Hashmap: " + word_count_neg);
 
 	}
 
@@ -123,33 +126,87 @@ public class SentAnalysis {
 
 	}
 
+	public static double log2(double N)
+    {
+        // calculate log2 N indirectly
+        // using log() method
+        double result = (Math.log(N) / Math.log(2));
+
+        return result;
+    }
 
 	/*
 	 * Classifier: Classifies the input text (type: String) as positive or negative
 	 */
-	public static String classify(String text)
-	{
+	public static String classify(String text){
+		//get all individual words & get them in right format
+		text = text.replaceAll("\\p{Punct}", "");
+		text = text.toLowerCase();
+
+		ArrayList<String> words = new ArrayList<String>();
+		Scanner s = new Scanner(text);
+		s.useDelimiter(" +");
+		while (s.hasNext()){
+			words.add(s.next());
+		}
+
 		//initialize our results to 0
 		double prob_result_pos = 0;
 		double prob_result_neg = 0;
 
-		//PROBABILITY TEXT IS POSITIVE SENTIMENT:
-
 		//calculate the Pr(positive) = #positive reviews / total # reviews
-		double pr_positive = ;
-
-
-
-		//PROBABILITY TEXT IS NEGATIVE SENTIMENT:
-
+		double pr_positive = (double)word_count_pos.size()/(double)(word_count_pos.size() + word_count_neg.size());
 		//calculate the Pr(negative) = #negative reviews / total # reviews
-		double pr_negative = ;
+		double pr_negative = (double)word_count_neg.size()/(double)(word_count_pos.size() + word_count_neg.size());
 
+
+		prob_result_pos = log2(pr_positive);
+		System.out.println("Initial: " + prob_result_pos);
+
+		prob_result_neg = log2(pr_negative);
+
+		double n_word_pos = 0;
+		double n_word_neg = 0;
+
+		for (int w = 0; w < words.size(); w++){
+			System.out.println("Word: " + words.get(w));
+			double curr_conditionalprob_neg = 0;
+			double curr_conditionalprob_pos = 0;
+			if (word_count_pos.get(words.get(w)) == null){
+				System.out.println(w + " is NULL in positive");
+				n_word_pos = 0;
+			}
+			if(word_count_neg.get(words.get(w)) == null){
+				System.out.println(w + " is NULL in negative");
+				n_word_neg = 0;
+			}
+			if (word_count_neg.get(words.get(w)) != null){
+				n_word_neg = word_count_neg.get(words.get(w));
+			}
+			if (word_count_pos.get(words.get(w)) != null) {
+				n_word_pos = word_count_pos.get(words.get(w));
+			}
+
+			double lambda = 0.0001;
+
+			curr_conditionalprob_neg = (n_word_neg + lambda) / (word_count_neg.size() + (words.size() * lambda));
+			System.out.println(curr_conditionalprob_neg);
+			curr_conditionalprob_pos = (n_word_pos + lambda) / (word_count_pos.size() + (words.size() * lambda));
+			System.out.println(curr_conditionalprob_pos);
+
+
+			prob_result_neg += log2(curr_conditionalprob_neg);
+			System.out.println("Cummulative Neg: " + prob_result_neg);
+			prob_result_pos += log2(curr_conditionalprob_pos);
+			System.out.println("Pos: " + prob_result_pos);
+
+
+		}
 
 
 		String result="";
 
-		if (prob_result_neg > prob_result_pos){
+		if (prob_result_neg >= prob_result_pos){
 			result = "negative";
 		}
 		else{
@@ -180,9 +237,21 @@ public class SentAnalysis {
 		ArrayList<String> filesToClassify = readFiles(folder);
 
 		// Iterate over all files in filesToClassify
-		// call classify on each individual file
-		// store that result in an ArrayList of "positive" and "negative" classifications
-		// return the ArrayList
+		for (int i = 0; i < filesToClassify.size(); i++){
+
+			String inputtext = "";
+
+			Path file = Paths.get(foldername+"/"+filesToClassify.get(i));
+			try {
+				inputtext = Files.readString(file);
+			}
+			catch (IOException e) {
+            	e.printStackTrace();
+        	}
+			String result = classify(inputtext);	//classify this input text as positive or negative
+			System.out.println("File: " + foldername + "/" + filesToClassify.get(i));	//print what file we're classifying
+			System.out.println("Result: " + result);	//print the classification
+		}
 
 	}
 
